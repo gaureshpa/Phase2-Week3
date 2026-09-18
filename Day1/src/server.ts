6bd4f7a93c8ffd7b8a8fc6f4714190714c728a0e
Gauresh P A <gauresh.pa@vonnue.com>
2026-09-18 16:14:31 +0530

    feat: add graceful shutdown of connection pool

--- Day1/src/server.ts
+++ Day1/src/server.ts
@@ -1,6 +1,17 @@
 import app from "./app.js";
+import pool from "./db/connection.js"
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
 });
 
+process.on("SIGINT", async () => {
+    console.log("Shutting down server...");
+
+
+    await pool.end();
+
+    console.log("Database connections closed");
+
+    process.exit(0);
+});
